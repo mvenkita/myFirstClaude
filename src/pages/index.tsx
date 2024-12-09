28b380765353e3b309c8e797236e74fc68e9424e
mvenkita <muthu@ligero-inc.com>
2024-12-09 18:08:32 -0500

    msgs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -421,7 +421,6 @@ const ModularApp = () => {
           Generate Shares
         </Button>
       </CardFooter>
-      {shares?.length > 0 && (
         <CardContent>
           <h3 className="text-lg font-semibold">Polynomial Coefficients:</h3>
           <ul className="list-disc pl-5">
@@ -440,7 +439,6 @@ const ModularApp = () => {
             ))}
           </ul>
         </CardContent>
-      )}
     </Card>
           </TabsContent>
  
